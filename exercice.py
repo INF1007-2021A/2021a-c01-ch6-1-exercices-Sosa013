f4e51bd8d35873f459472bf059f2ef742fc98433
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2021-09-28 17:15:59 -0700

    changement 1

--- exercice.py
+++ exercice.py
@@ -6,11 +6,9 @@ def order(values: list = None) -> list:
 
     if values is None:
         # TODO: demander les valeurs ici
-        pass
-        values = input("ENTREZ LES VALEURS SUIVANTES:").split( )
-
-    print (values)
-    return list
+        list = input('entrez 10 valejrs séparés par des espaces:').split()
+        print(list)
+        return list
 
 def anagrams(words: list = None) -> bool:
     if words is None:
@@ -34,24 +32,51 @@ def contains_doubles(items: list) -> bool:
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    best_student = dict()
+    for key, value in student_grades.items():
+        average = sum(value)/len(value)
+
+        if len(best_student) == 0 or (best_student.values())[0] < average:
+            best_student = {key: average}
+
+
+
+    return best_student
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
 
-    return {}
+    frequency = dict()
+
+    frequency = {letter: sentence.count(letter) for letter in sentence}
+
+    sorted_keys = sorted(frequency, key=frequency.__getitem__, reverse = True)
+    for key in sorted_keys:
+        if frequency [key] > 5:
+            print("le caractère {key} contient {frequency [key]}")
+    return frequency
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    name = input("Quel est le nom de votre recette").split()
+    ingredients = input("entrez la liste d'ingredients, Inserer les ingredients par une virgule").split()
+
+    return{name:ingredients}
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    name = input("Quel est le nom de la recette:").split()
+    if name in ingredients:
+        print(ingredients[name])
+
+    else:
+        print("Je ne connais pas la recette")
+        print_recipe()
+
 
 
 def main() -> None:
